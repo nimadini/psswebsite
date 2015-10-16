1925e772ee4e50ba02a168e6e24fbc0313db6a77
Nima Dini <nima.dini@utexas.edu>
2015-10-16 16:54:40 -0500

    basic layout done

--- app.js
+++ app.js
@@ -5,16 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/layout');
-var users = require('./routes/users');
+var main = require('./routes/main');
 
 var app = express();
 
-// view engine setup
+
+// ---swig expts start
 var swig = require('swig');
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views/');
+
+swig.setDefaults({
+    loader: swig.loaders.fs(__dirname + '/views'),
+    cache: false
+});
+
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,8 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', main);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,11 +45,15 @@ app.use(function(req, res, next) {
 
 // error handlers
 
+
+console.log("here");
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+      console.log("ERROR1");
     res.render('error', {
       message: err.message,
       error: err
@@ -52,7 +65,9 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+    console.log("ERROR2");
+
+    res.render('error', {
     message: err.message,
     error: {}
   });
