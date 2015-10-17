8c3542f23cade71a82e8def69d124046fbc0669a
Nima Dini <nima.dini@utexas.edu>
2015-10-17 16:46:13 -0500

    date picker added | angular

---
+++ public/assets/js/landing/app.js
@@ -0,0 +1,14 @@
+(function(){
+    var app = angular.module('pss', [ ]);
+
+    app.controller("PostController", function() {
+        this.posts = [{
+            title: 'Game Night',
+            content: 'This is a test post content'
+        },{
+            title: 'Game Night (2)',
+            content: 'This is a test post (2) content'
+        }
+        ];
+    });
+})();
