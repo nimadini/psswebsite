98b4106f7e5e73fb897dd738b4a09959e6fd65c8
Nima Dini <nima.dini@utexas.edu>
2015-12-09 23:04:47 -0600

    minor yalda update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var http = require('http');
  * Get port from environment and store in Express.
  */
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.set('port', port);
