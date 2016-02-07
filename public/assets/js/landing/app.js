b7d77cfc24e14519c66395d4baa860d9822323a2
Nima Dini <nima.dini@utexas.edu>
2016-02-07 16:32:43 -0600

    updated about page

--- public/assets/js/landing/app.js
+++ public/assets/js/landing/app.js
@@ -69,4 +69,27 @@ https://www.utexas.edu/universityunions/texas-union/scene/underground/facilities
 
         };
     });
+
+    app.controller('TabsCtrl', ['$scope', function ($scope) {
+            $scope.tabs = [{
+                title: 'One',
+                url: 'one.tpl.html'
+            }, {
+                title: 'Two',
+                url: 'two.tpl.html'
+            }, {
+                title: 'Three',
+                url: 'three.tpl.html'
+            }];
+
+            $scope.currentTab = 'one.tpl.html';
+
+            $scope.onClickTab = function (tab) {
+                $scope.currentTab = tab.url;
+            };
+
+            $scope.isActiveTab = function(tabUrl) {
+                return tabUrl == $scope.currentTab;
+            }
+        }]);
 })();
